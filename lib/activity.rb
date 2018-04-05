8c44b0abaaea1ac1455f8201fea53b5e8439b409
apoc64 <sschwedt@gmail.com>
2018-04-05 10:59:03 -0600

    acitivity exists

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,5 @@
 class Activity
+
+  
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,13 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def setup
+    @a = Activity.new
+  end
+
+  def test_it_exists
+    assert_instance_of Activity, @a
+  end
+
 end
